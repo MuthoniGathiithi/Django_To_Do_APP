dcff38f3e0ed7ee1d4e3533c7930d983d2d77c7e
Joyce <gathiithijoyce74@gmail.com>
2025-07-17 19:18:52 +0300

    updated add_task.html

--- todo/views.py
+++ todo/views.py
@@ -7,24 +7,21 @@ def show_tasks(request):
     selected_date = request.GET.get('date')
     if selected_date:
         selected_date_obj = date.fromisoformat(selected_date)
-        tasks = Task.objects.filter(date=selected_date_obj)
     else:
         selected_date_obj = date.today()
-        tasks = Task.objects.filter(date=selected_date_obj)
 
-    # Get tasks for the current week
-    start_week = selected_date_obj - timedelta(days=selected_date_obj.weekday())
-    end_week = start_week + timedelta(days=6)
-    weekly_tasks = Task.objects.filter(date__range=[start_week, end_week]).order_by('date')
+    # Get tasks for 3 consecutive days starting from selected_date_obj
+    end_date = selected_date_obj + timedelta(days=2)
+    consecutive_tasks = Task.objects.filter(date__range=[selected_date_obj, end_date]).order_by('date')
 
     return render(
         request,
         "add_task.html",
         {
-            'tasks': tasks,
+            'tasks': Task.objects.filter(date=selected_date_obj),
             'selected_date': selected_date_obj,
             'today': date.today(),
-            'weekly_tasks': weekly_tasks
+            'consecutive_tasks': consecutive_tasks
         }
     )
 
